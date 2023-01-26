cee87b2920c318737b84a917ebac7f1b4225515a
Sherry Tang <33537857+XinranTang@users.noreply.github.com>
2023-01-25 21:56:15 -0800

    Delete experiments directory

--- experiments/segmentation/unet/__init__.py
+++
@@ -1,6 +0,0 @@
-from unet.data_loading import BasicDataset, SyntheticDataset, MixDataset
-from unet.dice_score import dice_coeff, dice_loss, multiclass_dice_coeff, labelwise_dice_coeff
-from unet.evaluate import evaluate, mask_to_image, dice, labelwise_dice, iou, labelwise_iou
-from unet.unet import UNet
-
-__all__ = ['BasicDataset', 'SyntheticDataset', 'MixDataset', 'dice_coeff', 'dice_loss', 'dice', 'labelwise_dice', 'multiclass_dice_coeff', 'labelwise_dice_coeff', 'iou', 'labelwise_iou', 'evaluate', 'mask_to_image', 'UNet']
--- experiments/segmentation/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from utils.visualization import plot_img_and_mask
-
-__all__ = ['plot_img_and_mask']
